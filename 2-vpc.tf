17ba9fdd5f80ebd5458bc1122a31cef847987369
Thomas Bell <thomasjbell065@gmail.com>
2025-05-03 17:03:53 -0400

    removed multiple VPCs

--- 2-vpc.tf
+++ 2-vpc.tf
@@ -8,19 +8,3 @@ resource "google_compute_network" "twismnetwork" {
   mtu                             = 1460
   delete_default_routes_on_create = false
 }
-
-resource "google_compute_network" "twismnetwork2" {
-  name                            = "twismnetwork2"
-  routing_mode                    = "REGIONAL"
-  auto_create_subnetworks         = false
-  mtu                             = 1460
-  delete_default_routes_on_create = false
-}
-
-resource "google_compute_network" "twismnetwork3" {
-  name                            = "twismnetwork3"
-  routing_mode                    = "REGIONAL"
-  auto_create_subnetworks         = false
-  mtu                             = 1460
-  delete_default_routes_on_create = false
-}
